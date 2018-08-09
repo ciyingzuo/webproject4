84264204a11f8b8147158d2bded6bc057b6c0601
ciyingzuo <huntercyz@outlook.com>
2018-08-09 10:46:47 -0400

    a

--- src/app/course-navigator/course-navigator.component.ts
+++ src/app/course-navigator/course-navigator.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {CourseServiceClient} from '../services/course.service.client';
 import {Router} from '@angular/router';
+import {UserServiceClient} from '../services/user.service.client';
+import {SectionServiceClient} from '../services/section.service.client';
 
 @Component({
   selector: 'app-course-navigator',
@@ -14,12 +16,16 @@ export class CourseNavigatorComponent implements OnInit {
   currentModule = {};
   currentLesson = {topic: []};
   topic = {};
+  sections = [];
 
   constructor(private router: Router,
-              private courseService: CourseServiceClient) {
+              private sectionService: SectionServiceClient,
+              private courseService: CourseServiceClient,
+              private userService: UserServiceClient) {
   }
 
   selectCourse(course) {
+    console.log(this.courses)
     this.currentCourse = course;
     this.currentModule = {};
     this.currentLesson = {topic: []};
@@ -51,9 +57,47 @@ export class CourseNavigatorComponent implements OnInit {
   }
 
   ngOnInit() {
+    let crs = [];
     this.courseService
       .findAllCourses()
-      .then(courses => this.courses = courses);
+      .then(courses => {
+        this.courses = courses;
+        crs = courses;
+      });
+
+
+    this.userService.currentUser()
+      .then(user =>
+          this.sectionService.findSectionForUser(user._id)
+            .then(sections => {
+                for (let c of crs) {
+                  console.log("pr");
+                  c.visibility = 'false';
+                }
+
+                for (let c of crs) {
+                  for (let section of sections) {
+                    if (section.section.course == c.id || c.visibility == 'PUBLIC') {
+                      c.visibility = 'true';
+                    }
+                  }
+                }
+
+                this.courses = crs;
+              }
+            )
+        , err => {
+          for (let c of crs) {
+            if (c.visibility == 'PUBLIC') {
+              c.visibility = 'true';
+            } else {
+              c.visibility = 'false';
+            }
+          }
+          this.courses = crs;
+        });
+
+    // {'active': course.id == currentCourse.id}
   }
 
 }
